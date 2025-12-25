d8fe727d03b9b9ed20cc6ab60b0af57347b3b428
mansigeek <mansimgeek435@gmail.com>
2025-12-25 10:03:08 +0530

    Remove unused comp

--- components/layout-wrapper.tsx
+++ components/layout-wrapper.tsx
@@ -2,12 +2,9 @@
 
 import { ProgressProvider } from "@bprogress/next/app";
 
-import { Header } from "@/app/components/Header";
-
 function LayoutWrapper({ children }: { children: React.ReactNode }) {
     return (
         <ProgressProvider color="#ffffff" height="4px" options={{ showSpinner: false }} shallowRouting>
-            <Header />
             {children}
         </ProgressProvider>
     );
